e7864e7cf6154f4abc1873911a3fde90a132a9d8
ipb24mnevecorova-maker <ipb24.m.nevecorova@vtdt.edu.lv>
2025-10-27 11:10:10 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+  <script src="script.js"></script>
+
+   <div class="main">
+     <div class="header">
+       <div class="header-buttons">
+         <div class="logo">logo</div>
+         <div class="buttons">
+           <div class="home">about us</div>
+          
+
+
+       </div>
+     </div>
+     <div class="block">
+       <div class="recipeblocks">
+         <div class="mealtest">
+
+         </div>
+       </div>
+     </div>
+   </div>
+
+
+
+  </div>
+</body>
+
+</html>
